1134a5179b7f75e8e4d3d2c3f4eb5d155f89e427
Raphaël Velt <raphv@users.noreply.github.com>
2022-04-02 23:54:03 +0200

    UX improvements

--- game.js
+++ game.js
@@ -1,17 +1,16 @@
 const PAIRS = 3,
-	DEFAULT_FLAG = document.getElementById('flag-[LINE]-0').src,
-	DEFAULT_TOOLTIP = document.getElementById('pair-[LINE]-0').title,
+	DEFAULT_FLAG = "twemoji/temp.svg",
+	DEFAULT_TOOLTIP = "Type or click on a flag to fill in",
 	FLAGS = ['AC', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ', 'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CP', 'CR', 'CU', 'CV', 'CW', 'CX', 'CY', 'CZ', 'DE', 'DG', 'DJ', 'DK', 'DM', 'DO', 'DZ', 'EA', 'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET', 'EU', 'FI', 'FJ', 'FK', 'FM', 'FO', 'FR', 'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY', 'HK', 'HM', 'HN', 'HR', 'HT', 'HU', 'IC', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ', 'OM', 'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY', 'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SX', 'SY', 'SZ', 'TA', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ', 'UA', 'UG', 'UM', 'UN', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU', 'WF', 'WS', 'XK', 'YE', 'YT', 'ZA', 'ZM', 'ZW'],
 	COUNTRY_NAMES = {'AC': 'Ascension Island', 'AD': 'Andorra', 'AE': 'United Arab Emirates', 'AF': 'Afghanistan', 'AG': 'Antigua & Barbuda', 'AI': 'Anguilla', 'AL': 'Albania', 'AM': 'Armenia', 'AO': 'Angola', 'AQ': 'Antarctica', 'AR': 'Argentina', 'AS': 'American Samoa', 'AT': 'Austria', 'AU': 'Australia', 'AW': 'Aruba', 'AX': 'Åland Islands', 'AZ': 'Azerbaijan', 'BA': 'Bosnia & Herzegovina', 'BB': 'Barbados', 'BD': 'Bangladesh', 'BE': 'Belgium', 'BF': 'Burkina Faso', 'BG': 'Bulgaria', 'BH': 'Bahrain', 'BI': 'Burundi', 'BJ': 'Benin', 'BL': 'St. Barthélemy', 'BM': 'Bermuda', 'BN': 'Brunei', 'BO': 'Bolivia', 'BQ': 'Caribbean Netherlands', 'BR': 'Brazil', 'BS': 'Bahamas', 'BT': 'Bhutan', 'BV': 'Bouvet Island', 'BW': 'Botswana', 'BY': 'Belarus', 'BZ': 'Belize', 'CA': 'Canada', 'CC': 'Cocos (Keeling) Islands', 'CD': 'Congo - Kinshasa', 'CF': 'Central African Republic', 'CG': 'Congo - Brazzaville', 'CH': 'Switzerland', 'CI': 'Côte d’Ivoire', 'CK': 'Cook Islands', 'CL': 'Chile', 'CM': 'Cameroon', 'CN': 'China', 'CO': 'Colombia', 'CP': 'Clipperton Island', 'CR': 'Costa Rica', 'CU': 'Cuba', 'CV': 'Cape Verde', 'CW': 'Curaçao', 'CX': 'Christmas Island', 'CY': 'Cyprus', 'CZ': 'Czechia', 'DE': 'Germany', 'DG': 'Diego Garcia', 'DJ': 'Djibouti', 'DK': 'Denmark', 'DM': 'Dominica', 'DO': 'Dominican Republic', 'DZ': 'Algeria', 'EA': 'Ceuta & Melilla', 'EC': 'Ecuador', 'EE': 'Estonia', 'EG': 'Egypt', 'EH': 'Western Sahara', 'ER': 'Eritrea', 'ES': 'Spain', 'ET': 'Ethiopia', 'EU': 'European Union', 'FI': 'Finland', 'FJ': 'Fiji', 'FK': 'Falkland Islands', 'FM': 'Micronesia', 'FO': 'Faroe Islands', 'FR': 'France', 'GA': 'Gabon', 'GB': 'United Kingdom', 'GD': 'Grenada', 'GE': 'Georgia', 'GF': 'French Guiana', 'GG': 'Guernsey', 'GH': 'Ghana', 'GI': 'Gibraltar', 'GL': 'Greenland', 'GM': 'Gambia', 'GN': 'Guinea', 'GP': 'Guadeloupe', 'GQ': 'Equatorial Guinea', 'GR': 'Greece', 'GS': 'South Georgia & South Sandwich Islands', 'GT': 'Guatemala', 'GU': 'Guam', 'GW': 'Guinea-Bissau', 'GY': 'Guyana', 'HK': 'Hong Kong SAR China', 'HM': 'Heard & McDonald Islands', 'HN': 'Honduras', 'HR': 'Croatia', 'HT': 'Haiti', 'HU': 'Hungary', 'IC': 'Canary Islands', 'ID': 'Indonesia', 'IE': 'Ireland', 'IL': 'Israel', 'IM': 'Isle of Man', 'IN': 'India', 'IO': 'British Indian Ocean Territory', 'IQ': 'Iraq', 'IR': 'Iran', 'IS': 'Iceland', 'IT': 'Italy', 'JE': 'Jersey', 'JM': 'Jamaica', 'JO': 'Jordan', 'JP': 'Japan', 'KE': 'Kenya', 'KG': 'Kyrgyzstan', 'KH': 'Cambodia', 'KI': 'Kiribati', 'KM': 'Comoros', 'KN': 'St. Kitts & Nevis', 'KP': 'North Korea', 'KR': 'South Korea', 'KW': 'Kuwait', 'KY': 'Cayman Islands', 'KZ': 'Kazakhstan', 'LA': 'Laos', 'LB': 'Lebanon', 'LC': 'St. Lucia', 'LI': 'Liechtenstein', 'LK': 'Sri Lanka', 'LR': 'Liberia', 'LS': 'Lesotho', 'LT': 'Lithuania', 'LU': 'Luxembourg', 'LV': 'Latvia', 'LY': 'Libya', 'MA': 'Morocco', 'MC': 'Monaco', 'MD': 'Moldova', 'ME': 'Montenegro', 'MF': 'St. Martin', 'MG': 'Madagascar', 'MH': 'Marshall Islands', 'MK': 'North Macedonia', 'ML': 'Mali', 'MM': 'Myanmar (Burma)', 'MN': 'Mongolia', 'MO': 'Macao SAR China', 'MP': 'Northern Mariana Islands', 'MQ': 'Martinique', 'MR': 'Mauritania', 'MS': 'Montserrat', 'MT': 'Malta', 'MU': 'Mauritius', 'MV': 'Maldives', 'MW': 'Malawi', 'MX': 'Mexico', 'MY': 'Malaysia', 'MZ': 'Mozambique', 'NA': 'Namibia', 'NC': 'New Caledonia', 'NE': 'Niger', 'NF': 'Norfolk Island', 'NG': 'Nigeria', 'NI': 'Nicaragua', 'NL': 'Netherlands', 'NO': 'Norway', 'NP': 'Nepal', 'NR': 'Nauru', 'NU': 'Niue', 'NZ': 'New Zealand', 'OM': 'Oman', 'PA': 'Panama', 'PE': 'Peru', 'PF': 'French Polynesia', 'PG': 'Papua New Guinea', 'PH': 'Philippines', 'PK': 'Pakistan', 'PL': 'Poland', 'PM': 'St. Pierre & Miquelon', 'PN': 'Pitcairn Islands', 'PR': 'Puerto Rico', 'PS': 'Palestinian Territories', 'PT': 'Portugal', 'PW': 'Palau', 'PY': 'Paraguay', 'QA': 'Qatar', 'RE': 'Réunion', 'RO': 'Romania', 'RS': 'Serbia', 'RU': 'Russia', 'RW': 'Rwanda', 'SA': 'Saudi Arabia', 'SB': 'Solomon Islands', 'SC': 'Seychelles', 'SD': 'Sudan', 'SE': 'Sweden', 'SG': 'Singapore', 'SH': 'St. Helena', 'SI': 'Slovenia', 'SJ': 'Svalbard & Jan Mayen', 'SK': 'Slovakia', 'SL': 'Sierra Leone', 'SM': 'San Marino', 'SN': 'Senegal', 'SO': 'Somalia', 'SR': 'Suriname', 'SS': 'South Sudan', 'ST': 'São Tomé & Príncipe', 'SV': 'El Salvador', 'SX': 'Sint Maarten', 'SY': 'Syria', 'SZ': 'Eswatini', 'TA': 'Tristan da Cunha', 'TC': 'Turks & Caicos Islands', 'TD': 'Chad', 'TF': 'French Southern Territories', 'TG': 'Togo', 'TH': 'Thailand', 'TJ': 'Tajikistan', 'TK': 'Tokelau', 'TL': 'Timor-Leste', 'TM': 'Turkmenistan', 'TN': 'Tunisia', 'TO': 'Tonga', 'TR': 'Turkey', 'TT': 'Trinidad & Tobago', 'TV': 'Tuvalu', 'TW': 'Taiwan', 'TZ': 'Tanzania', 'UA': 'Ukraine', 'UG': 'Uganda', 'UM': 'U.S. Outlying Islands', 'UN': 'United Nations', 'US': 'United States', 'UY': 'Uruguay', 'UZ': 'Uzbekistan', 'VA': 'Vatican City', 'VC': 'St. Vincent & Grenadines', 'VE': 'Venezuela', 'VG': 'British Virgin Islands', 'VI': 'U.S. Virgin Islands', 'VN': 'Vietnam', 'VU': 'Vanuatu', 'WF': 'Wallis & Futuna', 'WS': 'Samoa', 'XK': 'Kosovo', 'YE': 'Yemen', 'YT': 'Mayotte', 'ZA': 'South Africa', 'ZM': 'Zambia', 'ZW': 'Zimbabwe'},
-	BASE_LETTER_CLASS = document.getElementById('letter-[LINE]-0').className,
-	BASE_PAIR_CLASS = document.getElementById('pair-[LINE]-0').className,
+	BASE_LETTER_CLASS = "guess-letter px-1 fs-2 lh-1 text-center border border-2 border-secondary rounded mx-1 mx-sm-2",
+	BASE_PAIR_CLASS = "border border-2 border-secondary rounded py-1 px-sm-1 py-lg-2 ms-1 ms-sm-2 ms-lg-3",
 	BASE_KEY_CLASS = document.querySelector('button.keyboard-letter').className,
 	CLASS_STATUS = [
 		' bg-dark text-light', ' bg-warning text-dark', ' bg-success text-light', ' bg-primary text-light',
 	],
 	SQUARE_STATUS = [ '⬛', '🟨', '🟩', '🟦' ],
 	MAIN_SECTION = document.querySelector('main'),
-	LINE_TEMPLATE = MAIN_SECTION.innerHTML,
 	WORD_TO_GUESS = WORD_LIST[Math.floor(Math.random()*WORD_LIST.length)],
 	SHARE_MODAL = new bootstrap.Modal(document.getElementById('success'));
 
@@ -31,14 +30,26 @@ function wordToEmojis(word) {
 function addLine() {
 	current_line++;
 	let newline = document.createElement('div');
-	newline.innerHTML = LINE_TEMPLATE.replaceAll('[LINE]', current_line);
+		contents = `<div class="d-flex align-items-center justify-content-center my-2 my-lg-3" id="line-${current_line}"><div class="lh-1 text-center"><p class="mb-0">Guess</p><h2>${1+current_line}</h2></div>`;
+	for (let i = 0; i < PAIRS; i++) {
+		contents += `<div class="${BASE_PAIR_CLASS}" id="pair-${current_line}-${i}" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${DEFAULT_TOOLTIP}">
+          <div class="d-flex">
+            <div class="${BASE_LETTER_CLASS}" type="text" id="letter-${current_line}-${2*i}">_</div>
+            <div class="${BASE_LETTER_CLASS}" type="text" id="letter-${current_line}-${2*i+1}">_</div>
+          </div>
+          <img class="d-block mx-auto mt-1" src="${DEFAULT_FLAG}" width="36" id="flag-${current_line}-${i}">
+        </div>`;
+	}
+    contents += '</div>';
+	newline.innerHTML = contents;
 	MAIN_SECTION.appendChild(newline);
 	for (let i = 0; i < PAIRS; i++) {
 		new bootstrap.Tooltip(
 			document.getElementById(`pair-${current_line}-${i}`)
 		);
 	}
-	showHints();
+	current_letter = 0;
+	resetRow();
 }
 
 function resetRow() {
@@ -138,6 +149,7 @@ function giveHint() {
 	}
 	if (hint_level >= 2*PAIRS) {
 		document.getElementById('hint-button').disabled = true;
+		document.getElementById('hint-level').textContent = '';
 	}
 }
 
@@ -193,7 +205,6 @@ function checkLine() {
 	for (let i = 0; i < PAIRS; i++) {
 		document.getElementById(`pair-${current_line}-${i}`).className = BASE_PAIR_CLASS + CLASS_STATUS[pair_statuses[i]];
 	}
-	addLine();
 	letter_statuses.forEach(function(s, i) {
 		let key_div = document.getElementById(`key-${guess[i]}`),
 			key_status = parseInt(key_div.getAttribute('data-status'));
@@ -209,8 +220,10 @@ function checkLine() {
 	}).join(''));
 	document.getElementById('check-button').disabled = true;
 	if (guess === WORD_TO_GUESS) {
+		document.getElementById('result-preview').innerHTML = document.getElementById(`line-${current_line}`).outerHTML;
 		SHARE_MODAL.show();
 	}
+	addLine();
 }
 
 function shareGame() {
@@ -258,7 +271,7 @@ for (let i = 0; i < 26; i++) {
 		let second_letter = String.fromCharCode(65+j),
 			pair = first_letter + second_letter;
 		if (FLAGS.indexOf(pair) == -1) {
-			flagboard_rows += `<td class="flagboard-empty"><img src="twemoji/forbidden.svg" alt="Invalid flag" width="16"></td>`;
+			flagboard_rows += `<td class="bg-secondary bg-opacity-10 border"></td>`;
 		} else {
 			let country_name = COUNTRY_NAMES[pair];
 			flagboard_rows += `<td class="flagboard-cell" id="flagcell-${pair}" data-letters="${pair}">
